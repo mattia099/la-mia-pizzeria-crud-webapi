828dbc0b162627dd48407d4414f0e10f28d4007a
mattiapuglisi099@gmail.com <mattiapuglisi099@gmail.com>
2022-07-26 12:10:47 +0200

    api controller - metodo Get()

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,21 @@
+﻿using la_mia_pizzeria_razor_layout.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_razor_layout.Controllers
+{
+    [Route("api/pizzas")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            using (PizzaContext context = new PizzaContext())
+            {
+                List<Pizza> pizzas = context.Pizza.ToList<Pizza>();
+                return Ok(pizzas);
+            }
+        }
+    }
+}
